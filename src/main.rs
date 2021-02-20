07db89c909d01dc837e5e047d4e3efc3bd4c5e76
Jb DOYON <jb@jiby.tech>
2021-02-20 20:43:32 +0000

    Read ascii dump of arbitrary files
    Main now parses args to show usage and read file content into vector.
    
    Change dependency test-temp-file with assert_fs, which gives temp
    folder and more popularity/support.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,22 @@
+use std::env;
+use std::fs::File;
+use std::io::Read;
+use std::process;
+
 fn main() {
-    println!("Hello, world!");
+    // Check arguments for file to open
+    let args: Vec<String> = env::args().collect();
+    if args.len() < 2 {
+        println!("Usage: {} FILE_TO_SEND", args[0]);
+        process::exit(1);
+    }
+    let mut file = match File::open(&args[1]) {
+        Ok(f) => f,
+        Err(err) => panic!("File error: {}", err),
+    };
+
+    // Read the file and print contents (ASCII numbers)
+    let mut buffer = Vec::<u8>::new();
+    file.read_to_end(&mut buffer).unwrap();
+    println!("{:?}", buffer);
 }
--- tests/integration_tests.rs
+++ tests/integration_tests.rs
@@ -1,36 +1,27 @@
 use assert_cmd::Command;
+use assert_fs::prelude::*;
 use rand::Rng;
-use std::io::Read;
-use std::io::Seek;
-use std::io::SeekFrom;
-use std::io::Write;
-use test_temp_file::TestTempFile;
 
 // Scenario: qrxfil on random file generates PNG files
 #[test]
 fn file_to_qr_happy() {
     // Given a file with a few KB of random data
-    let original_filename = "Test_file.bin";
-    let mut tmp_file = TestTempFile::new(original_filename.to_string());
+    let temp = assert_fs::TempDir::new().unwrap();
+    let input_file = temp.child("to_send.bin");
 
+    // Fill input file with random data
     let size_bytes: i32 = 1024; // 1KB right now
     let mut rng = rand::thread_rng();
     let random_chars: Vec<u8> = (0..size_bytes).map(|_| rng.gen_range(0..255)).collect();
-    tmp_file.write_all(&random_chars).unwrap();
-
-    // Rewind to read back the numbers
-    let _s = tmp_file.seek(SeekFrom::Start(0));
-    // read the whole file
-    let mut buffer = Vec::new();
-    tmp_file.read_to_end(&mut buffer).unwrap();
-    println!("{:?}", buffer);
+    input_file.write_binary(&random_chars).unwrap();
 
     // When running qrxfil with it
     let mut cmd = Command::cargo_bin("qrxfil").unwrap();
     // Then exit code is zero for success
-    cmd.assert().success();
+    cmd.arg(input_file.path()).assert().success();
     // Then a folder is named after the file
     // And folder contains dozens of files
     // And the files are all valid PNG
-    assert_eq!(1, 1);
+
+    temp.close().unwrap(); // clean up the temp folder
 }
