289dc8c742acc33133214ce2e0903d5f1024271c
Jb DOYON <jb@jiby.tech>
2021-02-21 19:50:05 +0000

    Add output folder populating

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,20 @@
 use std::env;
-use std::fs::File;
+use std::fs;
 use std::io::Read;
+use std::io::Write;
+use std::path::Path;
 use std::process;
 
 fn main() {
     // Check arguments for file to open
     let args: Vec<String> = env::args().collect();
-    if args.len() < 2 {
-        println!("Usage: {} FILE_TO_SEND", args[0]);
+    if args.len() < 3 {
+        println!("Usage: {} FILE_TO_SEND OUTPUT_FOLDER", args[0]);
         process::exit(1);
     }
-    let mut file = match File::open(&args[1]) {
+    let input_filename = &args[1];
+    let output_folder = &args[2];
+    let mut file = match fs::File::open(input_filename) {
         Ok(f) => f,
         Err(err) => panic!("File error: {}", err),
     };
@@ -20,4 +24,15 @@ fn main() {
     file.read_to_end(&mut buffer)
         .expect("Error reading input file");
     println!("{:?}", buffer);
+
+    // Ensure output folder exists
+    fs::create_dir_all(output_folder).expect("Could not create/check output folder");
+    let output_path = Path::new(output_folder);
+    let mut output_file = match fs::File::create(output_path.join("1.png")) {
+        Ok(f) => f,
+        Err(err) => panic!("File error: {}", err),
+    };
+    output_file
+        .write_all(&buffer)
+        .expect("Error populating the folder with binary content");
 }
--- tests/integration_tests.rs
+++ tests/integration_tests.rs
@@ -1,5 +1,6 @@
 use assert_cmd::Command;
 use assert_fs::prelude::*;
+use predicates::prelude::*;
 use rand::Rng;
 
 // Scenario: qrxfil on random file generates PNG files
@@ -8,6 +9,7 @@ fn file_to_qr_happy() {
     // Given a file with a few KB of random data
     let temp = assert_fs::TempDir::new().unwrap();
     let input_file = temp.child("to_send.bin");
+    let output_folder = temp.child("output_qrs");
 
     // Fill input file with random data
     let size_bytes: i32 = 1024; // 1KB right now
@@ -17,14 +19,22 @@ fn file_to_qr_happy() {
         .write_binary(&random_chars)
         .expect("Could not write random to file for test seeding");
 
-    // When running qrxfil with it
-    let mut cmd = Command::cargo_bin("qrxfil").expect("Error launching qrxfil command");
+    // When running qrxfil with input filename + output folder
+    let mut cmd = Command::cargo_bin("qrxfil").expect("Error find qrxfil command");
     // Then exit code is zero for success
-    cmd.arg(input_file.path()).assert().success();
-    // Then a folder is named after the file
-
-    // And folder contains dozens of files
-    // And the files are all valid PNG
-
-    temp.close().expect("Error deleting temporary folder"); // clean up the temp folder
+    cmd.args(&[input_file.path(), output_folder.path()])
+        .assert()
+        .success();
+    // Then a folder is created
+    output_folder.assert(predicate::path::is_dir());
+    // And folder contains files
+    let output_files =
+        std::fs::read_dir(output_folder.path()).expect("Could not list output directory");
+    assert_ne!(
+        output_files.count(),
+        0,
+        "Should have found files inside output directory"
+    );
+    // clean up the temp folder
+    temp.close().expect("Error deleting temporary folder");
 }
