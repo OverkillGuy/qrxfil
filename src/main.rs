82f0143cf5aa4fa0ad248300c36820a383326a7f
Jb DOYON <jb@jiby.tech>
2021-02-22 19:31:42 +0000

    Add stubby chunk writing via BufReader

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,7 @@ use std::env;
 use std::fs;
 use std::io::Read;
 use std::io::Write;
+// use std::io::{BufRead, BufReader};
 use std::io::{Seek, SeekFrom};
 use std::path::Path;
 use std::process;
@@ -27,7 +28,7 @@ fn main() {
     fs::create_dir_all(output_folder).expect("Could not create/check output folder");
     let output_path = Path::new(output_folder);
     // Create a base64 version of our file
-    let mut output_file = match fs::File::create(output_path.join("input_b64.txt")) {
+    let mut base64_file = match fs::File::create(output_path.join("input_b64.txt")) {
         Ok(f) => f,
         Err(err) => panic!("File error: {}", err),
     };
@@ -41,14 +42,39 @@ fn main() {
 
         // Write base64 version of what we read
         let mut base64_encoder =
-            base64::write::EncoderWriter::new(&mut output_file, base64::STANDARD);
+            base64::write::EncoderWriter::new(&mut base64_file, base64::STANDARD);
 
         base64_encoder
             .write_all(&raw_data_buffer)
             .expect("Error writing base64 of input file");
     }
+    // Measure file length from where we are at the end
+    let base64_filesize_bytes = base64_file
+        .seek(SeekFrom::End(0))
+        .expect("Error checking base64 filesize");
+    println!("File size: {}", base64_filesize_bytes);
+
     // rewind to the start of base64 encoded file to fan-out into chunks
-    output_file
+    base64_file
         .seek(SeekFrom::Start(0))
         .expect("Error rewinding base64 file before chunking");
+
+    base64_file
+        .sync_all()
+        .expect("Error syncing base64 file to disks");
+
+    // let base64_filesize_bytes = base64_file
+    //     .stream_len()
+    //     .expect("Error checking base64 filesize");
+
+    // let chunk_reader;
+    // let chunk_read;
+    // {
+    //     chunk_reader = BufReader::with_capacity(1024, base64_file);
+
+    //     chunk_read = chunk_reader
+    //         .fill_buf()
+    //         .expect("Error reading chunk off base64 file");
+    // }
+    // println!("Read {:?} bytes: {:?}", chunk_read, chunk_reader);
 }
