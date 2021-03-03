05ef55038d5eea7274f4f76fb453eaa1411cfaeb
Jb DOYON <jb@jiby.tech>
2021-03-03 19:12:40 +0000

    Refactor encode to function

--- src/main.rs
+++ src/main.rs
@@ -11,11 +11,108 @@ use std::fs;
 use std::io::Read;
 use std::io::Write;
 use std::io::{Seek, SeekFrom};
-use std::path::Path;
+use std::path::{Path, PathBuf};
 extern crate base64;
 extern crate clap;
 extern crate image;
 extern crate qrcode;
+
+fn encode(input_file: PathBuf, output_folder: PathBuf) {
+    let mut input_file = match fs::File::open(input_file) {
+        Ok(f) => f,
+        Err(err) => panic!("File error: {}", err),
+    };
+
+    // Ensure output folder exists
+    fs::create_dir_all(output_folder.as_path()).expect("Could not create/check output folder");
+    // Create a base64 version of our file
+    let mut base64_file = match fs::File::create(output_folder.as_path().join("input_b64.txt")) {
+        Ok(f) => f,
+        Err(err) => panic!("File error: {}", err),
+    };
+
+    // Read the input file and write it out as base64
+    {
+        // Separate scope to drop the encoder
+        let mut raw_data_buffer = Vec::<u8>::new();
+        input_file
+            .read_to_end(&mut raw_data_buffer)
+            .expect("Error reading input file");
+
+        // Write base64 version of what we read
+        let mut base64_encoder =
+            base64::write::EncoderWriter::new(&mut base64_file, base64::STANDARD);
+
+        base64_encoder
+            .write_all(&raw_data_buffer)
+            .expect("Error writing base64 of input file");
+    }
+    // Measure file length from where we are at the end
+    let base64_filesize_bytes = base64_file
+        .seek(SeekFrom::End(0))
+        .expect("Error checking base64 filesize");
+
+    base64_file
+        .sync_data()
+        .expect("Error syncing base64 file to disk");
+
+    let mut base64_file = fs::File::open(output_folder.join("input_b64.txt"))
+        .expect("Error reopening the base64 file to chunk");
+
+    let chunk_size: usize = 1024; // 1 KB
+
+    #[allow(clippy::cast_precision_loss)]
+    let chunk_totals = (base64_filesize_bytes as f64 / (chunk_size as f64)).ceil(); // round UP on f64 division
+    println!(
+        "File {:?}. base64 size: {} bytes = {} chunks of 1KB",
+        input_file, base64_filesize_bytes, chunk_totals
+    );
+    let mut chunk_count = 1;
+    let header_size = format!("{:02}OF{:02}", 1, 10).len();
+    let expected_chunk_bytes_read = chunk_size - header_size;
+    loop {
+        let mut chunk_header: Vec<u8> =
+            format!("{:02}OF{:02}", chunk_count, chunk_totals).into_bytes();
+
+        let mut chunk = Vec::<u8>::with_capacity(chunk_size);
+        chunk.append(&mut chunk_header); // FIXME write prefix to buffer
+
+        let bytes_read_chunk = std::io::Read::by_ref(&mut base64_file)
+            .take(expected_chunk_bytes_read as u64)
+            .read_to_end(&mut chunk)
+            .expect("Error reading chunk off file");
+        if bytes_read_chunk == 0 {
+            break;
+        }
+
+        // Encode some data into bits.
+        let code = QrCode::new(&chunk).expect("Error encoding chunk into QR code");
+
+        // Render the bits into an image.
+        let image = code.render::<Luma<u8>>().build();
+
+        // Save the image.
+        image
+            .save(output_folder.join(format!("{:02}.png", chunk_count)))
+            .expect("Error saving chunk's QR code file");
+
+        println!("Saved QR {:02}/{}", chunk_count, chunk_totals);
+        // let mut out_file = fs::File::create())
+
+        // out_file
+        //     .write(chunk_header.as_bytes())
+        //     .expect("Error writing out file chunk header");
+        // out_file
+        //     .write_all(&chunk)
+        //     .expect("Error writing out file chunk");
+        chunk_count += 1;
+    }
+    println!(
+        "Split file in {} QR chunks, in folder {:?}",
+        chunk_count - 1,
+        output_folder
+    );
+}
 
 fn main() {
     let matches = App::new("qrxfil")
@@ -48,99 +145,9 @@ fn main() {
 
     let input_filename = matches_exfil.value_of("input").unwrap();
     let output_folder = matches_exfil.value_of("output_folder").unwrap();
-    let mut input_file = match fs::File::open(input_filename) {
-        Ok(f) => f,
-        Err(err) => panic!("File error: {}", err),
-    };
 
-    // Ensure output folder exists
-    fs::create_dir_all(output_folder).expect("Could not create/check output folder");
-    let output_path = Path::new(output_folder);
-    // Create a base64 version of our file
-    let mut base64_file = match fs::File::create(output_path.join("input_b64.txt")) {
-        Ok(f) => f,
-        Err(err) => panic!("File error: {}", err),
-    };
-
-    // Read the input file and write it out as base64
-    {
-        // Separate scope to drop the encoder
-        let mut raw_data_buffer = Vec::<u8>::new();
-        input_file
-            .read_to_end(&mut raw_data_buffer)
-            .expect("Error reading input file");
-
-        // Write base64 version of what we read
-        let mut base64_encoder =
-            base64::write::EncoderWriter::new(&mut base64_file, base64::STANDARD);
-
-        base64_encoder
-            .write_all(&raw_data_buffer)
-            .expect("Error writing base64 of input file");
-    }
-    // Measure file length from where we are at the end
-    let base64_filesize_bytes = base64_file
-        .seek(SeekFrom::End(0))
-        .expect("Error checking base64 filesize");
-
-    base64_file
-        .sync_data()
-        .expect("Error syncing base64 file to disk");
-
-    let mut base64_file = fs::File::open(output_path.join("input_b64.txt"))
-        .expect("Error reopening the base64 file to chunk");
-
-    let chunk_size: usize = 1024; // 1 KB
-
-    #[allow(clippy::cast_precision_loss)]
-    let chunk_totals = (base64_filesize_bytes as f64 / (chunk_size as f64)).ceil(); // round UP on f64 division
-    println!(
-        "File {}. base64 size: {} bytes = {} chunks of 1KB",
-        input_filename, base64_filesize_bytes, chunk_totals
-    );
-    let mut chunk_count = 1;
-    let header_size = format!("{:02}OF{:02}", 1, 10).len();
-    let expected_chunk_bytes_read = chunk_size - header_size;
-    loop {
-        let mut chunk_header: Vec<u8> =
-            format!("{:02}OF{:02}", chunk_count, chunk_totals).into_bytes();
-
-        let mut chunk = Vec::<u8>::with_capacity(chunk_size);
-        chunk.append(&mut chunk_header); // FIXME write prefix to buffer
-
-        let bytes_read_chunk = std::io::Read::by_ref(&mut base64_file)
-            .take(expected_chunk_bytes_read as u64)
-            .read_to_end(&mut chunk)
-            .expect("Error reading chunk off file");
-        if bytes_read_chunk == 0 {
-            break;
-        }
-
-        // Encode some data into bits.
-        let code = QrCode::new(&chunk).expect("Error encoding chunk into QR code");
-
-        // Render the bits into an image.
-        let image = code.render::<Luma<u8>>().build();
-
-        // Save the image.
-        image
-            .save(output_path.join(format!("{:02}.png", chunk_count)))
-            .expect("Error saving chunk's QR code file");
-
-        println!("Saved QR {:02}/{}", chunk_count, chunk_totals);
-        // let mut out_file = fs::File::create())
-
-        // out_file
-        //     .write(chunk_header.as_bytes())
-        //     .expect("Error writing out file chunk header");
-        // out_file
-        //     .write_all(&chunk)
-        //     .expect("Error writing out file chunk");
-        chunk_count += 1;
-    }
-    println!(
-        "Split file in {} QR chunks, in folder {:?}",
-        chunk_count - 1,
-        output_path
+    encode(
+        Path::new(input_filename).to_path_buf(),
+        Path::new(output_folder).to_path_buf(),
     );
 }
