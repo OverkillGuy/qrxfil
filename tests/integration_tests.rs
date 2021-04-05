48cef7d1c0d640275193a0e44e8f257bb8775a60
Jb DOYON <jb@jiby.tech>
2021-04-05 19:25:31 +0100

    Add PDF generation test
    Catches issues with PDF not generated or generated without any
    chunk-related pages
    
    Scenario: qrxfil on random file generates PDF file
      Given a file with a few KB of random data
      When running qrxfil in PDF exfil-mode with input filename + output folder
      Then exit code is zero for success
      Then a PDF is created
      And the PDF has more than 1 page

--- tests/integration_tests.rs
+++ tests/integration_tests.rs
@@ -65,6 +65,44 @@ fn file_to_qr_happy() {
     temp.close().expect("Error deleting temporary folder");
 }
 
+// Scenario: qrxfil on random file generates PDF file
+#[test]
+fn file_to_pdf_happy() {
+    // Given a file with a few KB of random data
+    let temp = assert_fs::TempDir::new().unwrap();
+    let input_file = temp.child("to_send.bin");
+    let output_folder = temp.child("output_qrs");
+
+    // Fill input file with random data
+    let size_bytes: i32 = 1024; // 1KB right now
+    let mut rng = rand::thread_rng();
+    let random_chars: Vec<u8> = (0..size_bytes).map(|_| rng.gen_range(0..255)).collect();
+    input_file
+        .write_binary(&random_chars)
+        .expect("Could not write random to file for test seeding");
+
+    // When running qrxfil in PDF exfil-mode with input filename + output folder
+    let mut cmd = Command::cargo_bin("qrxfil").expect("Error find qrxfil command");
+    // Then exit code is zero for success
+    let args = [
+        "pdfprint",
+        input_file.path().to_str().unwrap(),
+        output_folder.path().to_str().unwrap(),
+    ];
+    cmd.args(&args).assert().success();
+    // Then a PDF is created
+    let pdf_filepath = output_folder.child("output.pdf");
+    pdf_filepath.assert(predicate::path::is_file());
+    // And the PDF has more than 1 page
+    let pdf_file = pdf::file::File::<Vec<u8>>::open(&pdf_filepath.path()).unwrap();
+    assert!(
+        pdf_file.num_pages() > 1,
+        "PDF must have more than guard page"
+    );
+    // clean up the temp folder
+    temp.close().expect("Error deleting temporary folder");
+}
+
 // Scenario: qrxfil restore --csv
 #[test]
 fn csv_restored_happy() {
